787d98e69d728b6fe845ab30bd383eea313b1504
cOsMiCTr <onurbaki@me.com>
2022-07-10 23:10:11 +0200

    Done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Betamax-Angular-client';
+  title = 'myFlix-Angular-client';
+
+  constructor(
+    public dialog: MatDialog
+  ) { }
+
+  // This is the function responsible for opening the user login form
+  openUserRegistrationDialog(): void {
+    this.dialog.open(UserRegistrationFormComponent, {
+
+      width: '280px'
+    });
+  }
+
+  // This is the function responsible for opening the user login form
+  openUserLoginDialog(): void {
+    this.dialog.open(UserLoginFormComponent, {
+      // Assign dialog width
+      width: '280px'
+    });
+  }
 }
